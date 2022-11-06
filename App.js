ae19dc3b54184044258dbc1e026d7de9aa12bff7
ChoudharyAditya <117287999+ChoudharyAditya@users.noreply.github.com>
2022-11-06 22:19:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 import "./styles.css";
 import Navbar from "./Navbar.jsx";
 import {Routes, Route} from "react-router-dom";
-// import Order from "./Order";
 import Home from "./Home.js";
-import Form from "./Form.js";
-
-// <Home />
-//     <Form />
-
 export default function App() {
   return (<div>
     <Navbar />
@@ -19,4 +13,3 @@ export default function App() {
     </div>
  );
 }
-// <Route  path="/Order" element={<Form />} />
