c6b89c9cbffe88560d7d5731127d0e82105a9e47
ChoudharyAditya <117287999+ChoudharyAditya@users.noreply.github.com>
2022-11-06 22:17:56 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -4,10 +4,6 @@ import { useNavigate} from 'react-router-dom';
 function Navbar()  {
   const navigate = useNavigate();
 
-  const navigateToOrder = () => {
-    navigate('/Order');
-  };
-
   const navigateHome = () => {
     navigate('/');
   };
@@ -16,7 +12,6 @@ function Navbar()  {
     <div className="Nav-bar">
       <h1 id="nav-heading">SMART-SHOP "U"</h1>
       <button id="home" onClick={navigateHome}  type="button">Home</button>
-      <button id="order" onClick={navigateToOrder} type="button">Order</button>
       </div>
   );
 };
